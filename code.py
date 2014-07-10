5ffeae7fb9a79f224b3db539d7545c1a7516acf6
cBping <4527756802qq.com>
2014-07-11 00:24:56 +0800

    init the db

--- code.py
+++ code.py
@@ -3,6 +3,7 @@
 import config
 import log
 import sever
+import conn
 
 # 配置文件初始化
 log.log_I("read the config file:")
@@ -14,6 +15,11 @@ log.log_D(config.get_config("LOG_PATH") + config.get_config("LOG_FILE_NAME"))
 log.initLog(1, "web", config.get_config("LOG_PATH") + config.get_config("LOG_FILE_NAME"))
 
 log.log_D(config.get_config("LOG_PATH") + config.get_config("LOG_FILE_NAME"))
+
+
+log.log_D("init the db:")
+conn.setWebConfig(dbn="mysql",db=config.get_config("DB_NAME"),user=config.get_config("DB_USER"),
+                  pw=config.get_config("DB_PW"),host=config.get_config("DB_HOST"),port=config.get_config("DB_PORT"))
 
 if __name__ == '__main__':
     sever.run()
