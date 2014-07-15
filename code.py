81a150ae59e5bd1ae780d2be56ae3d93bdb7b2fa
cBping <4527756802qq.com>
2014-07-15 22:58:05 +0800

    do web with session
    添加session功能

--- code.py
+++ code.py
@@ -11,7 +11,7 @@ pjPath = os.path.dirname(os.path.abspath(__file__))
 import python.config as config
 import python.log as log
 import python.conn as conn
-import python.sever as server
+import python.server as server
 
 
 # 配置文件初始化
@@ -38,20 +38,55 @@ conn.setWebConfig(dbn="mysql",
 
 import web
 
-# 加个处理器(等同中间件)
-def my_processor(handler):
-    # log.log_D('before handling')
-    result = handler()
-    # log.log_D('after handling')
-    return result
+# web.config
+
+# web.config.session_parameters['cookie_name'] = 'webpy_session_id'
+# web.config.session_parameters['cookie_domain'] = None
+web.config.session_parameters['timeout'] = 20,  # in seconds
+web.config.session_parameters['ignore_expiry'] = True
+# web.config.session_parameters['ignore_change_ip'] = True
+# web.config.session_parameters['secret_key'] = 'fLjUfxqXtfNoIldA0A0J'
+# web.config.session_parameters['expired_message'] = 'Session expired'
 
 
-# 启动wsgi服务
 try:
 
     log.log_D("app run:")
+    # 应用初始
     app = web.application(server.URLS, globals(), autoreload=True)
-    #app.add_processor(my_processor)
+
+    web.config.debug = False
+
+    # session处理
+    log.log_D("session init:")
+    session = web.session.Session(app, web.session.DiskStore(os.path.join(pjPath, 'sessions')),
+                                  initializer={'count': 0, "login": False,
+                                               "user_id": "", "user_account": "",
+                                               "user_name": ""})
+
+    def my_processor(handler):
+        """
+        加个处理器(等同中间件)
+        路由为/l/*,则校验是否登陆，为登录，则返回404（请查看python.globals.code）
+        """
+        if web.ctx.fullpath.split("/")[1].lower() == "l":
+            from python.globals.Res import Res
+
+            if web.ctx.session.get("login", False) == False:
+                return Res("404").dict()
+
+        result = handler()
+        return result
+
+    # 钩子
+    def session_hook():
+        web.ctx.session = session
+
+    app.add_processor(web.loadhook(session_hook))
+    app.add_processor(my_processor)
+
+
+    # 启动wsgi服务
     application = app.wsgifunc()
 
 except Exception, e:
